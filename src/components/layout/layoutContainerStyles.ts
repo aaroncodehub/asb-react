f7677d394bcbab688318e19fe621b2f41d05feea
aaron zhang <aaron.zhang@orionhealth.com>
2022-02-13 08:24:38 +1300

    feat: optimize responsive design for mobile, tablet and desktop

--- src/components/layout/layoutContainerStyles.ts
+++ src/components/layout/layoutContainerStyles.ts
@@ -1,15 +1,15 @@
 import { Theme } from '@material-ui/core/styles';
 export const layoutContainerStyles = (theme: Theme) => ({
     root: {
-      [theme.breakpoints.up("sm")]: {
+      [theme.breakpoints.up("xs")]: {
         maxWidth: "375px",
       },
-      [theme.breakpoints.up("md")]: {
+      [theme.breakpoints.up("sm")]: {
         maxWidth: "768px",
       },
   
-      [theme.breakpoints.up("lg")]: {
-        maxWidth: "1024px",
+      [theme.breakpoints.up("md")]: {
+        maxWidth: "1152px",
       },
     },
   });  
