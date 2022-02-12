135c334430e7dd600aa090cb220d6627c144bb8b
aaron zhang <aaron.zhang@orionhealth.com>
2022-02-12 22:48:30 +1300

    feat: add lazy loading to improve performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import React, { Suspense, lazy } from 'react';
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./components/theme/theme";
 
-import Form from "./components/form/Form";
-import NavigationMenu from "./components/navigationMenu/NavigationMenu";
-import Welcome from "./components/welcome/Welcome";
+const NavigationMenu = lazy(() => import("./components/navigationMenu/NavigationMenu"))
+const Welcome = lazy( () => import("./components/welcome/Welcome"))
+const Form = lazy(() => import("./components/form/Form"))
 
 const App = () => (
   <ThemeProvider theme={theme}>
+    <Suspense fallback={<div>loading ...</div>}>
     <NavigationMenu />
     <Welcome/>
       <Form />
+    </Suspense>
   </ThemeProvider>
 );
 
